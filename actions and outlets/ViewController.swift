42434fe2f70ebc203ec65400464023f9b67ac12e
Kailah Boston <kailahboston@gmail.com>
2020-08-01 13:34:24 -0400

    actions and outlets

---
+++ actions and outlets/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  actions and outlets
+//
+//  Created by Apple on 7/22/20.
+//  Copyright © 2020 Kailah Boston. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var App: [UILabel]!
+    
+    @IBOutlet weak var input: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
